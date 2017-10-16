71747a1603d4925cf72f2f7d87cb8870a16d0852
Alexandre Resende <alexandre.amres@gmail.com>
2017-10-16 09:28:53 -0200

    feat: Created module for user

--- api/routers/user.router.js
+++ api/routers/user.router.js
@@ -1,10 +1,24 @@
 
 const path = require('path');
 const express = require('express');
+
 const router = express.Router();
+const models = require(__dirname + '/../../models/index.js');
 
 router.get('/users', (req, res) => {
     res.send({done: 'Hello'});
+});
+
+router.post('/users', (req, res) => {
+
+    models.User.create({
+        name: req.body.name,
+        email:req.body.email,
+        password: req.body.password
+    }).then(user => {
+        res.json(user);
+    });
+
 });
 
 module.exports = router;
